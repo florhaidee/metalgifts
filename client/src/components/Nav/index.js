fcfe5140a668404523dc4a206bfeacfca476857f
Florhaidee <florhaideedev@gmail.com>
2020-11-11 12:41:47 -0700

    initial files

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -43,8 +43,8 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          <img src='/images/Logo.png' alt="icon" />
+           Steel Gifts c.m
         </Link>
       </h1>
 
